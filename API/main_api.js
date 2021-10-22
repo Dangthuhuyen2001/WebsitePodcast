90b938107299c3c5eef773939e6c54d1c480e8bc
Huyenahihi <thuuhuyenn2001@gmail.com>
2021-10-22 12:07:27 +0700

    Done UI

---
+++ API/main_api.js
@@ -0,0 +1,10 @@
+// let callapi = 'http://localhost:3000/posts'
+// fetch(callapi)
+//   .then((response) => {
+//     return response.json()
+//   })
+//   .then(callback)
+//   .catch((error) => {
+//     alert(error)
+//   })
+// export const callapi = 'http://localhost:3000/posts'
